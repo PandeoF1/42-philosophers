4d5133766a1cdc23a2efce1b1e9ded0d8b7c42c4
tnard <theo.nard18@gmail.com>
2022-02-14 11:15:46 +0100

    done

--- philo/srcs/ft_thread.c
+++ philo/srcs/ft_thread.c
@@ -6,7 +6,7 @@
 /*   By: tnard <tnard@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/29 14:54:33 by tnard             #+#    #+#             */
-/*   Updated: 2022/02/03 12:43:49 by tnard            ###   ########lyon.fr   */
+/*   Updated: 2022/02/14 11:15:15 by tnard            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,6 +52,7 @@ void	*ft_thread(void *ph)
 
 	x = 0;
 	philos = (t_philos *)ph;
+	philos->time_to_die = get_time();
 	while (philos->master->status != -1)
 	{
 		if (philos->master->status == 0)
@@ -75,28 +76,28 @@ void	*ft_thread(void *ph)
 
 void	ft_create_thread(t_philo *philo, int i)
 {
-	t_philos	*philos;
+	t_philos	*phi;
 
-	philos = malloc(sizeof(t_philos));
-	philo->philos = philos;
-	philos->id = i;
-	philos->time_to_die = get_time();
-	pthread_mutex_init(&philos->fork_right, NULL);
-	philos->master = philo;
-	if (pthread_create(&philo->thread[i], NULL, ft_thread, (void *)philos))
+	phi = malloc(sizeof(t_philos));
+	philo->philos = phi;
+	phi->id = i;
+	pthread_mutex_init(&phi->fork_right, NULL);
+	phi->master = philo;
+	phi->status = 0;
+	if (pthread_create(&philo->thread[i - 1], NULL, ft_thread, (void *)phi))
 		ft_printf("Error: pthread_create\n");
-	pthread_detach(philo->thread[i]);
+	pthread_detach(philo->thread[i - 1]);
 	while (i++ < philo->nb_philo)
 	{
-		philos->next = malloc(sizeof(t_philos));
-		philos->next->id = i;
-		pthread_mutex_init(&philos->next->fork_right, NULL);
-		philos->next->fork_left = &philos->fork_right;
-		philos->next->time_to_die = get_time();
-		philos->next->master = philo;
-		philos = philos->next;
-		if (pthread_create(&philo->thread[i], NULL, ft_thread, (void *)philos))
+		phi->next = malloc(sizeof(t_philos));
+		phi->next->id = i;
+		phi->next->status = 0;
+		pthread_mutex_init(&phi->next->fork_right, NULL);
+		phi->next->fork_left = &phi->fork_right;
+		phi->next->master = philo;
+		phi = phi->next;
+		if (pthread_create(&philo->thread[i - 1], NULL, ft_thread, (void *)phi))
 			ft_printf("Error: pthread_create\n");
-		pthread_detach(philo->thread[i]);
-	}	
+		pthread_detach(philo->thread[i - 1]);
+	}
 }
